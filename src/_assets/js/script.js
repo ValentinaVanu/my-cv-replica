cb8b3c1dd92da3250475b9cea97776e137b01794
unknown <vanuvalentina16@gmail.com>
2020-09-06 21:47:59 +0100

    My cv replica Refreshed

---
+++ src/_assets/js/script.js
@@ -0,0 +1,18 @@
+// function toggleMenu(){
+//   var menuBtn = document.getElementById('menu-btn');
+//   var menuContent = document.getElementById('menu-content');
+//     if(menuContent.style.display =="block"){
+//     menuContent.style.display = "none";
+//     menuBtn.style.color = 'white';
+//     menuBtn.classList.add('icon__menu');
+//     menuBtn.classList.remove('icon__cancel');
+//   } else {
+//     menuContent.style.display ="block";
+//     menuContent.classList.add("newMenuStyle");
+//     menuBtn.classList.remove('icon__menu');
+//     menuBtn.classList.add('icon__cancel');
+//     menuBtn.style.color = 'rgb(84, 74, 161)';
+//   }
+// }
+// alert(5)
+
